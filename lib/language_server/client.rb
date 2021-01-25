c6f8545001383e37dcd4116182548bdfea4ce3c1
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2021-01-24 17:09:03 -0700

    Add some boilerplate for LanguageServer modules

---
+++ lib/language_server/client.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "language_server/client/gem_version"
+
+module LanguageServer
+  # Module for LanguageServer Client-related code.
+  module Client
+  end
+end
---
+++ lib/language_server/protocol.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "language_server/protocol/gem_version"
+
+module LanguageServer
+  # Module for LanguageServer Protocol-related code.
+  module Protocol
+  end
+end
