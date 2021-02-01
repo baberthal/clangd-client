b45e77688b4ce97f57dd44a4996c3d984feba6b7
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2021-01-31 20:31:23 -0700

    Add platform-specific specs for LS::Protocol
    Add specs to describe LS::Protocol.uri_to_file_path on unix-like and
    windows platforms.

--- spec/language_server/protocol_spec.rb
+++ spec/language_server/protocol_spec.rb
@@ -2,21 +2,27 @@
 
 RSpec.describe LanguageServer::Protocol do
   describe ".uri_to_file_path" do
-    it "raises InvalidURIError if the args are malformed" do
-      expect { described_class.uri_to_file_path("test") }
-        .to raise_error LanguageServer::Protocol::InvalidURIError
+    context "when on unix", if: ClangdClient.unix? do
+      it "raises InvalidURIError if the args are malformed" do
+        expect { described_class.uri_to_file_path("test") }
+          .to raise_error LanguageServer::Protocol::InvalidURIError
+      end
+
+      it "properly parses the uri with a file:/ uri" do
+        expect(
+          described_class.uri_to_file_path("file:/usr/local/test/test.test")
+        ).to eq "/usr/local/test/test.test"
+      end
+
+      it "properly parses the uri with a file:/// uri" do
+        expect(
+          described_class.uri_to_file_path("file:///usr/local/test/test.test")
+        ).to eq "/usr/local/test/test.test"
+      end
     end
 
-    it "properly parses the uri with a file:/ uri" do
-      expect(
-        described_class.uri_to_file_path("file:/usr/local/test/test.test")
-      ).to eq "/usr/local/test/test.test"
-    end
-
-    it "properly parses the uri with a file:/// uri" do
-      expect(
-        described_class.uri_to_file_path("file:///usr/local/test/test.test")
-      ).to eq "/usr/local/test/test.test"
+    context "when on windows", if: ClangdClient.windows? do
+      pending "Add some windows examples"
     end
   end
 
