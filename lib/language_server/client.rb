83e6151af830a1c972791b047f97d5a129c1999b
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2021-02-01 11:13:56 -0700

    Update requires

--- lib/language_server/client.rb
+++ lib/language_server/client.rb
@@ -3,6 +3,10 @@
 require "concurrent"
 
 require "language_server/client/gem_version"
+require "language_server/client/base"
+require "language_server/client/errors"
+require "language_server/client/language_server_connection"
+require "language_server/client/response"
 
 module LanguageServer
   # Module for LanguageServer Client-related code.
